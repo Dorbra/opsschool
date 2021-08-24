c409216e38de8a71929e2071e0f2fad74e1f3da5
iSDorbra <doron.brand@ironsrc.com>
2021-08-24 17:53:32 +0300

    exercises 4+5

--- vagrant/fixScripts/exercise4-fix_server1.sh
+++ vagrant/fixScripts/exercise4-fix_server1.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #add fix to exercise4-server1 here
+
 # generate a SSH, created on ~/.ssh/id_rsa.pub
 ssh-keygen
-# copy ssh key for shared file pub key
-cp /home/vagrant/.ssh/id_rsa.pub /vagrant/server1pub.key
-
+echo '192.168.100.11 server2' | sudo tee -a /etc/hosts
--- vagrant/fixScripts/exercise4-fix_server2.sh
+++ vagrant/fixScripts/exercise4-fix_server2.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #add fix to exercise4-server2 here
 
-# copy-append server1 public key to authorized_keys file
-cat /vagrant/server1.key | tee -a /home/vagrant/.ssh/authorized_keys
+# generate a SSH, created on ~/.ssh/id_rsa.pub
+ssh-keygen
+echo '192.168.100.10 server1' | sudo tee -a /etc/hosts
